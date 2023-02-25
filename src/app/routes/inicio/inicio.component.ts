684eb8992be1a5a846277682455be0d81e43b6b9
EdgarAndres1 <119905721+EdgarAndres1@users.noreply.github.com>
2023-02-24 22:45:30 -0500

    Creacion rama 4.1 interfaces
    Se crean interfaces de cards

--- src/app/routes/inicio/inicio.component.ts
+++ src/app/routes/inicio/inicio.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { movieserie } from 'src/app/shared/interfaces/movieseries.interfaces';
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
@@ -7,4 +7,71 @@ import { Component } from '@angular/core';
 })
 export class InicioComponent {
 
+movies_series:movieserie[] = [
+  {
+    id : 1,
+  name: "Money Heist",
+  description : "Accion",
+  image : "/assets/5-money-heist.svg",
+  rating :0,
+  category : "movie"
+},
+{
+  id : 2,
+name: "Friends",
+description : "Humor",
+image : "/assets/6-friends.svg",
+rating :0,
+category : "serie"
+},
+{
+  id : 3,
+name: "The Big Bang Theory",
+description : "Humor",
+image : "/assets/7-the-big-bang-theory.svg",
+rating :0,
+category : "serie"
+},
+{
+  id : 4,
+name: "Two and a Half Men",
+description : "Humor",
+image : "/assets/8-two-and-a-half-men.svg",
+rating :0,
+category : "serie"
+},
+{
+  id : 5,
+name: "Black Widow",
+description : "Accion",
+image : "/assets/1-black-widow.svg",
+rating :0,
+category : "movie"
+},
+{
+  id : 6,
+name: "Shang-Chi",
+description : "Accion",
+image : "/assets/2-shang-chi.svg",
+rating :0,
+category : "movie"
+},
+{
+  id : 7,
+name: "Loki",
+description : "Accion ",
+image : "/assets/3-loki.svg",
+rating :0,
+category : "movie"
+},
+{
+  id : 8,
+name: "How i met your mother",
+description : "Humor ",
+image : "/assets/4-how-i-met-your-mother.svg",
+rating :0,
+category : "movie"
+}
+
+]
 }
---
+++ src/app/shared/interfaces/movieseries.interfaces.ts
@@ -0,0 +1,8 @@
+export  interface movieserie {
+    id : number;
+    name : string;
+    description : string;
+    image : string;
+    rating : number;
+    category : string;
+  }
